b98da4ce5f3451b250aa2bc7c3236c0503f15999
jc <jiowchern@gmail.com>
2022-10-03 11:12:14 +0800

    add sample scene.

---
+++ Scripts/Entry.cs
@@ -0,0 +1,17 @@
+﻿using Regulus.Remote;
+using UnityEngine;
+namespace PinionCyber.NetCode
+{
+    public class Entry : MonoBehaviour ,  Regulus.Remote.IEntry, IEchoable
+    {
+        void Regulus.Remote.IBinderProvider.AssignBinder(Regulus.Remote.IBinder binder)
+        {
+            binder.Bind<IEchoable>(this);
+        }
+
+        Value<string> IEchoable.Echo(string message)
+        {
+            return "re:" + message;
+        }
+    }
+}
