9bab7f60ab335f0648d920b3d45e36ebbe8d236c
jc <jiowchern@yahoo.com.tw>
2022-11-02 11:21:35 +0800

    clean.

---
+++ PinionCyber.NetCode/Assets/PinionCyber.NetCode/Sources/Editor/Startup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+using UnityEditor.Compilation;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace PinionCyber.NetCode
+{
+   
+    public static class Startup
+    {
+        
+    }
+
+}
